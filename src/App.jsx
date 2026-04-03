470badaf088df88f756b078bd716cdcf6c28240d
Muhammad Affan <affanmuhammad926@gmail.com>
2026-04-03 13:16:22 +0500

    Import changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
-import Layout from './components/Layout';
+import Layout from "./Components/layout"
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import MoodTracker from './pages/MoodTracker';
